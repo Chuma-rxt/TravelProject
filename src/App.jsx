13caa25b63f2f73dcc7096540b911dfa7466f648
Chuma-rxt <craxoti60@gmail.com>
2024-02-29 13:28:32 +0200

    adding default styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css"
+import Destinations from "./Components/Destinations/Destination";
+import Home from "./Components/Home/Home";
+import Middle from "./Components/Middle/Middle";
+import Navbar from "./Components/Navbar/Navbar";
+import Portfolio from "./Components/Portfolio/Portfolio";
+import Reviews from "./Components/Reviews/Reviews";
+import Questions from "./Components/Questions/Questions";
+import Subscribe from "./Components/Subscribe/Subscribe";
+import Footer from "./Components/Footer/Footer";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <Navbar />
+      {/* <Home />
+      <Middle />
+      <Destinations/>
+      <Portfolio/>
+      <Reviews/>
+      <Questions/>
+      <Subscribe/>
+      <Footer/> */}
+
+
+
+    </div>
+   
   )
 }
 
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import './Navbar.css'
+
+// Imported Icons
+import { RiBusLine } from "react-icons/ri";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { PiDotsNineBold } from "react-icons/pi";
+
+const Navbar = () => {
+  return (
+    <div className='navBar'>
+      <div className='logoDiv'>
+       <RiBusLine className='icon' />
+       <span>Chuma.R-Trips</span>
+      </div>
+
+      <div className='menu'>
+        <ul>
+          <li className='navList'>
+            Destination
+          </li>
+          <li className='navList'>
+            About Us
+          </li>
+          <li className='navList'>
+            Testimonial
+          </li>
+          <li className='navList'>
+            Gallery
+          </li>
+        </ul>
+
+        {/* icons to remove navbar */}
+        <AiFillCloseCircle className='icons' />
+      </div>
+
+      <button className='signUpBtn btn'>Sign Up</button>
+
+      <PiDotsNineBold className='icon' />
+       
+    </div>
+  )
+}
+
+export default Navbar
