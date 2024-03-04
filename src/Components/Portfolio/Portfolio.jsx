1e98debb734096c407d0f785dc15ecd27ee95bd1
Chuma-rxt <craxoti60@gmail.com>
2024-03-04 11:25:09 +0200

    adding my portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -2,10 +2,75 @@ import React from 'react'
 import './Portfolio.css'
 import './Portfolio.scss';
 
+import icon1 from '../../Assets/icon1.jpg'
+import icon2 from '../../Assets/destination.png'
+import icon3 from '../../Assets/24-hours-support.png'
+import image from '../../Assets/Untitled design.png';
+
 const Portfolio = () => {
   return (
     <div className='portfolio section container'>
-      
+      <div className='secContainer grid'>
+        <div className='leftContent'>
+          <div className='secHeading'>
+            <h3>Why Should You Choose Us</h3>
+            <p>
+              We have extensive knowledge and experience in the travel industry.
+            </p>
+          </div>
+
+          <div className='grid'>
+            <div className='singlePortifolio flex'>
+              <div className='iconDiv'>
+                <img src={icon1} alt='Icon Image' />
+              </div>
+
+              <div className='infor'>
+                <h4>Safety and Support</h4>
+                <p>
+                  Our top priority is the safety and well-being of our clients.
+                  We maintain high safety standards and have emergency support
+                  available during the trip.
+                </p>
+              </div>
+
+            </div>
+
+            <div className='singlePortifolio flex'>
+              <div className='iconDiv'>
+                <img src={icon2} alt='Icon Image' />
+              </div>
+
+              <div className='infor'>
+                <h4>Diverse Range of Destination</h4>
+                <p>
+                  Whether it's a domestic tour or an international adventure, we
+                  cover a wide range of destinations to cater to different interests
+                  and preferences.
+                </p>
+              </div>
+            </div>
+
+            <div className='singlePortifolio flex'>
+              <div className='iconDiv'>
+                <img src={icon3} alt='Icon Image' />
+              </div>
+
+              <div className='infor'>
+                <h4>24/7 Customer Support</h4>
+                <p>
+                  Our dedicated customer support team is avvailable round the clock to 
+                  address any quries or concerns before, during, and after the trip.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className='rightContent'>
+          <img src={image} alt='Image' />
+        </div>
+      </div>
     </div>
   )
 }
