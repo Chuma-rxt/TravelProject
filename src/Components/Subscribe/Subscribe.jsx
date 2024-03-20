8aacc7541c6c1182b4544f1f1d51759fed15ad13
Chuma-rxt <craxoti60@gmail.com>
2024-03-20 09:32:11 +0200

    The subscription

--- src/Components/Subscribe/Subscribe.jsx
+++ src/Components/Subscribe/Subscribe.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Subscribe.css'
 import './Subscribe.scss'
 
 // importing pictures
 import image1 from '../../Assets/call-removebg-preview.png'
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Subscribe = () => {
+
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
+
   return (
     <div className='subscribe section container'>
       <div className='secContainer grid'>
-        <img src={image1} alt='Div Image' />
+        <img src={image1} alt='Div Image' data-aos= 'fade-up' />
 
         <div className='textDiv'>
-          <h4>Best Way To Start Your Journey!</h4>
-          <p>
+          <h4 data-aos= 'fade-up'> Best Way To Start Your Journey!</h4>
+          <p data-aos= 'fade-up'>
             We offer personalized itineraries tailored to individual preferences and interest
           </p>
-          <button className='btn'>Start Here</button>
+          <button className='btn' data-aos= 'fade-up'>Start Here</button>
         </div>
       </div>
     </div>
