e29d83637c9027b8b0bc36f609dbd6e2f8ce051f
Chuma-rxt <craxoti60@gmail.com>
2024-03-20 09:10:18 +0200

    the Portfolio

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Portfolio.css'
 import './Portfolio.scss';
 
@@ -7,26 +7,34 @@ import icon2 from '../../Assets/destination1.jpg'
 import icon3 from '../../Assets/7d0e6ce6ff877a4d4cbc7f2349a0a428.jpg'
 import image from '../../Assets/Untitled design.png';
 
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 const Portfolio = () => {
+
+  useEffect(()=>{
+    Aos.init({duration: 2000})
+  },[])
+
   return (
     <div className='portfolio section container'>
       <div className='secContainer grid'>
         <div className='leftContent'>
           <div className='secHeading'>
-            <h3>Why Should You Choose Us</h3>
-            <p>
+            <h3 data-aos= 'fade-up'>Why Should You Choose Us</h3>
+            <p data-aos= 'fade-up'>
               We have extensive knowledge and experience in the travel industry.
             </p>
           </div>
 
-          <div className='grid'>
+          <div className='grid' data-aos= 'fade-up'>
             <div className='singlePortfolio flex'>
-              <div className='iconDiv'>
+              <div className='iconDiv' data-aos= 'fade-up'>
                 <img src={icon1} alt='Icon Image' />
               </div>
 
               <div className='infor'>
-                <h4>Safety and Support</h4>
+                <h4 data-aos= 'fade-up'>Safety and Support</h4>
                 <p>
                  The security and welfare of our customers come first.
                  Throughout the tour, we have emergency backup available
@@ -37,12 +45,12 @@ const Portfolio = () => {
             </div>
 
             <div className='singlePortfolio flex'>
-              <div className='iconDiv'>
+              <div className='iconDiv' data-aos= 'fade-up'>
                 <img src={icon2} alt='Icon Image' />
               </div>
 
               <div className='infor'>
-                <h4>Diverse Range of Destination</h4>
+                <h4 data-aos= 'fade-up'>Diverse Range of Destination</h4>
                 <p>
                  We offer a wide selection of sites to accommodate a 
                  variety of interests and tastes, whether it's a domestic
@@ -52,12 +60,12 @@ const Portfolio = () => {
             </div>
 
             <div className='singlePortfolio flex'>
-              <div className='iconDiv'>
+              <div className='iconDiv' data-aos= 'fade-up'>
                 <img src={icon3} alt='Icon Image' />
               </div>
 
               <div className='infor'>
-                <h4>24/7 Customer Support</h4>
+                <h4 data-aos= 'fade-up'>24/7 Customer Support</h4>
                 <p>
                  Before, during, and after the trip, our committed customer 
                  support team is available around-the-clock to answer any 
@@ -68,7 +76,7 @@ const Portfolio = () => {
           </div>
         </div>
 
-        <div className='rightContent'>
+        <div className='rightContent' data-aos= 'fade-down'>
           <img src={image} alt='Image' />
         </div>
       </div>
